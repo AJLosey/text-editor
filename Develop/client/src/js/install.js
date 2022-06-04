6f5d6852922f7bbb304829d2a9a19e87055fead6
AJLosey <aaronlosey11@gmail.com>
2022-06-03 20:22:44 -0500

    fixed logo and install

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -1,15 +1,17 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let installEvent
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     butInstall.style.visibility = 'visible';
+    installEvent = event;
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async (event) => {
-    await event.prompt();
+    await installEvent.prompt();
     installBtn.setAttribute('disabled', true);
     installBtn.textContent = 'Installed!';
 });
@@ -18,3 +20,5 @@ butInstall.addEventListener('click', async (event) => {
 window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
 });
+
+console.log(butInstall, window)
